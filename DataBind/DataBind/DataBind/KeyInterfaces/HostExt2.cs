486c99856c1a6da1d4c06e0081b35eefb95a8948
windyuuy <kugoo11v12@126.com>
2022-01-19 10:16:44 +0800

    feature: 支持完整的构建流程

--- DataBind/DataBind/DataBind/KeyInterfaces/HostExt2.cs
+++ DataBind/DataBind/DataBind/KeyInterfaces/HostExt2.cs
@@ -6,6 +6,9 @@ namespace DataBinding
 {
 	using number = System.Double;
 
+	/// <summary>
+	/// IStdHost 代理
+	/// </summary>
 	public static class HostExt2
 	{
 		/**
@@ -14,6 +17,26 @@ namespace DataBinding
          * @param cb 重新执行后，发生变化则会出发回调函数
          */
 		public static Watcher Watch(this IStdHost self, CombineType<object, string, Func<object, object, object>> expOrFn, Action<object, object, object> cb, CombineType<object, string, number, bool> loseValue = null, bool sync = false)
+		{
+			return HostExt._Swatch((IHostAccessor)self, expOrFn, cb, loseValue, sync);
+		}
+
+		/**
+         * 侦听一个数据发生的变化
+         * @param expOrFn 访问的数据路径，或数据值的计算函数，当路径中的变量或计算函数所访问的值发生变化时，将会被重新执行
+         * @param cb 重新执行后，发生变化则会出发回调函数
+         */
+		public static Watcher Watch(this IStdHost self, string expOrFn, Action<object, object, object> cb, CombineType<object, string, number, bool> loseValue = null, bool sync = false)
+		{
+			return HostExt._Swatch((IHostAccessor)self, expOrFn, cb, loseValue, sync);
+		}
+
+		/**
+         * 侦听一个数据发生的变化
+         * @param expOrFn 访问的数据路径，或数据值的计算函数，当路径中的变量或计算函数所访问的值发生变化时，将会被重新执行
+         * @param cb 重新执行后，发生变化则会出发回调函数
+         */
+		public static Watcher Watch(this IStdHost self, Func<object, object, object> expOrFn, Action<object, object, object> cb, CombineType<object, string, number, bool> loseValue = null, bool sync = false)
 		{
 			return HostExt._Swatch((IHostAccessor)self, expOrFn, cb, loseValue, sync);
 		}
