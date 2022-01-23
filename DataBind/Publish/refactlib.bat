341170edebce33a7224b749b63cb757c14d4f86d
windyuuy <kugoo11v12@126.com>
2022-01-23 23:19:56 +0800

    feature: 支持重组构建流程

---
+++ DataBind/Publish/refactlib.bat
@@ -0,0 +1,19 @@
+SET Source=%1
+SET Bin=%1\publish\DataBind
+IF NOT EXIST %Bin%\DataBind MKDIR %Bin%\DataBind
+IF NOT EXIST %Bin%\Editor MKDIR %Bin%\Editor
+IF NOT EXIST %Bin%\Editor\Entry MKDIR %Bin%\Editor\Entry
+IF NOT EXIST %Bin%\Editor\Service MKDIR %Bin%\Editor\Service
+
+XCOPY %Source%\CiLin.dll %Bin%\Editor\Service /y
+XCOPY %Source%\CiLin.pdb %Bin%\Editor\Service /y
+XCOPY %Source%\DataBindService.dll %Bin%\Editor\Service /y
+XCOPY %Source%\DataBindService.pdb %Bin%\Editor\Service /y
+XCOPY %Source%\Mono.Cecil.dll %Bin%\Editor\Service /y
+
+XCOPY %Source%\UnityDataBindService.dll %Bin%\Editor\Entry /y
+
+XCOPY %Source%\DataBind.dll %Bin%\DataBind /y
+XCOPY %Source%\DataBind.xml %Bin%\DataBind /y
+XCOPY %Source%\EngineAdapter.dll %Bin%\DataBind /y
+XCOPY %Source%\EngineAdapter.pdb %Bin%\DataBind /y
